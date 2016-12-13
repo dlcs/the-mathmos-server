772b0666513b0944f04c69720a0099de30a484a4
sarahcox1 <sarah.cox@digirati.com>
2016-12-13 10:45:28 +0000

    updated README and run-indexer.sh for pygar and barbarella

--- pygar/src/main/resources/scripts/run-indexer.sh
+++ pygar/src/main/resources/scripts/run-indexer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ANNOTATION_INDEXER_HOME=/home/digirati/annotation-search-server/dev/annotation-indexer/pygar
-java -cp $ANNOTATION_INDEXER_HOME/pygar.jar:$ANNOTATION_INDEXER_HOME/lib/*:$ANNOTATION_INDEXER_HOME/config/:$ANNOTATION_INDEXER_HOME/. com.digirati.pygar.mapping.IndexAWSMessages --Dpygar-aws-consumer.properties=file:/home/digirati/annotation-search-server/dev/annotation-indexer/pygar-aws-consumer.properties
+java -cp $ANNOTATION_INDEXER_HOME/pygar.jar:$ANNOTATION_INDEXER_HOME/lib/*:$ANNOTATION_INDEXER_HOME/config/:$ANNOTATION_INDEXER_HOME/. com.digirati.pygar.mapping.IndexAWSMessages -Dpygar-aws-consumer.properties=file:/home/digirati/annotation-search-server/dev/annotation-indexer/pygar-aws-consumer.properties
 
 
