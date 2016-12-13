6398c53bb8d8ecf3db427071542fb70de2cfeae4
sarahcox1 <sarah.cox@digirati.com>
2016-12-13 08:56:12 +0000

    moved the aws-consumer-properties to be a JVM argument

--- barbarella/src/main/java/com/digirati/barbarella/config/MainConfig.java
+++ barbarella/src/main/java/com/digirati/barbarella/config/MainConfig.java
@@ -22,7 +22,7 @@ import org.springframework.util.MethodInvoker;
 @Configuration
 @EnableScheduling
 @SuppressWarnings("deprecation")
-@PropertySource(MainConfig.CONSUMER_PROPERTIES_FILE)
+@PropertySource("${barbarella-aws-consumer.properties}")
 @ComponentScan(basePackages = {MainConfig.SERVICE_PACKAGE})
 @EnableElasticsearchRepositories(basePackages = MainConfig.REPOSITORY_PACKAGE)
 
@@ -30,12 +30,10 @@ public class MainConfig {
     
     private static final Logger LOG = Logger.getLogger(MainConfig.class);
 
-    public static final String CONSUMER_PROPERTIES_FILE = "classpath:aws-consumer.properties";
-
     
-    public static final String SERVICE_PACKAGE = "uk.co.digirati.text";
+    public static final String SERVICE_PACKAGE = "com.digirati.barbarella";
     
-    public static final String REPOSITORY_PACKAGE = "uk.co.digirati.text.search.repository";
+    public static final String REPOSITORY_PACKAGE = "com.digirati.barbarella.repository";
     
 
     @Autowired
