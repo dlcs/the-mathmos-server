6d5a031f7c135434ab915236e340e6aee25e4db4
sarahcox1 <sarah.cox@digirati.com>
2017-06-27 12:21:09 +0100

    ContextCompletetion fixed for match pygar and barbarella one

--- the-mathmos-server/src/main/java/com/digirati/themathmos/model/ContextCompletion.java
+++ the-mathmos-server/src/main/java/com/digirati/themathmos/model/ContextCompletion.java
@@ -1,23 +1,61 @@
 package com.digirati.themathmos.model;
 
-import org.springframework.data.elasticsearch.core.completion.Completion;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(value = JsonInclude.Include.NON_NULL)
-public class ContextCompletion extends Completion{
+public class ContextCompletion {
 
-  
+    private String[] input;
+    private String output;
+    private Integer weight;
+    private Object payload;
     private Object context;
-    
-    public ContextCompletion(String[] input) {
-	super(input);
+
+    private ContextCompletion() {
+	// required by mapper to instantiate object
     }
+
+    public ContextCompletion(String[] input) {
+	this.input = input;
+    }
+
     public Object getContext() {
 	return context;
     }
+
     public void setContext(Object context) {
 	this.context = context;
     }
 
+    public String[] getInput() {
+	return input;
+    }
+
+    public void setInput(String[] input) {
+	this.input = input;
+    }
+
+    public String getOutput() {
+	return output;
+    }
+
+    public void setOutput(String output) {
+	this.output = output;
+    }
+
+    public Object getPayload() {
+	return payload;
+    }
+
+    public void setPayload(Object payload) {
+	this.payload = payload;
+    }
+
+    public Integer getWeight() {
+	return weight;
+    }
+
+    public void setWeight(Integer weight) {
+	this.weight = weight;
+    }
 }
