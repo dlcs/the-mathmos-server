ad9bd559bbc6d4b6dd074187f6a23669ddbf7070
sarahcox1 <sarah.cox@digirati.com>
2017-08-17 10:06:40 +0100

    testing pre-commit

--- the-mathmos-server/src/main/java/com/digirati/themathmos/AnnotationSearchConstants.java
+++ the-mathmos-server/src/main/java/com/digirati/themathmos/AnnotationSearchConstants.java
@@ -3,41 +3,40 @@ package com.digirati.themathmos;
 public class AnnotationSearchConstants {
 
     public static final String FIELD_MOTIVATIONS = "motivations";
-    
+
     public static final String PARAM_FIELD_MOTIVATION = "motivation";
-    
-    public static final String PARAM_FIELD_DATE = "date"; 
+
+    public static final String PARAM_FIELD_DATE = "date";
     public static final String PARAM_FIELD_USER = "user";
-    
+
     public static final String PARAM_FIELD_QUERY = "q";
-    
+
     public static final String PARAM_FIELD_PAGE = "page";
-    
+
     public static final String PARAM_FIELD_WIDTH = "width";
     public static final String PARAM_FIELD_HEIGHT = "height";
 
     public static final int DEFAULT_PAGING_NUMBER = 100;
-    
+
     public static final int MAX_NUMBER_OF_HITS_RETURNED = 1000;
-    
+
     public static final String PAINTING_MOTIVATION = "painting";
-    
+
     public static final String NON_PAINTING_MOTIVATION = "non-painting";
-    
+
     public static final String EMPTY_QUERY_MESSAGE = "Please enter either a query, motivation, date or user to search";
-    
+
     public static final String EMPTY_AUTOCOMPLETE_QUERY_MESSAGE = "Please enter a query to search";
-    
-    
+
     public static final String TEXT_INDEX_NAME = "text_index";
-    
+
     public static final String W3C_INDEX_NAME = "w3cannotation";
-    
+
     public static final String CONTEXT_MANIFEST_NAME = "manifest";
-    
+
     public static final boolean USE_PARED_DOWN_OA = true;
-    
-    private AnnotationSearchConstants(){
-	
+
+    private AnnotationSearchConstants() {
+
     }
 }
